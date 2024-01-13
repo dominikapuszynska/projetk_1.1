b92d4883ef447c566758d0a85d344005b6d83a76
Dominika Puszynska <dpuszynska21@gmail.com>
2024-01-13 16:07:55 +0100

    human detection on image

---
+++ main.py
@@ -0,0 +1,33 @@
+import cv2
+
+img = cv2.imread('ludzie2.png')
+img= cv2.resize(img, (700, 500))
+
+print(type(img))
+print(img.shape)
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+# load image
+image = cv2.imread('ludzie2.png')
+image = cv2.resize(image, (700, 400))
+
+# detect people in the image
+(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+
+# draw the bounding boxes
+for (x, y, w, h) in rects:
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+print(f'Found {len(rects)} humans')
+
+# show the output images
+cv2.imshow("People detector", image)
+cv2.waitKey(0)
+
